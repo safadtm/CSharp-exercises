fbd2cefef6ae857b77aa5abe55c68bd36517f359
safadtm <safadtm2004@gmail.com>
2024-07-26 06:50:05 +0530

    Read Line

---
+++ ReadLine/ReadLine/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ReadLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name : ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello "+name); // String concatenation
+            Console.WriteLine("Hello {0}",name); // Composite formatting
+            Console.WriteLine($"Hello {name}"); // String interpolation 
+
+        }
+    }
+}
