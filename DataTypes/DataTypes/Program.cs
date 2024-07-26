799196027edaea3aee1cf3d0457663b6a0e26f2b
safadtm <safadtm2004@gmail.com>
2024-07-26 07:33:53 +0530

    Data Types

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1. Value Data Type
+             *    a. Predefined Data Type
+             *    b. User Defined Data Type
+             * 2. Pointer Data Type  
+             * 3. Reference Data Type
+             *    a. Predefined Data Type
+             *    b. User Defined Data Type
+             */
+
+
+            char c = 'A';
+            int i = 1200;
+            bool b = true;
+            Console.WriteLine(c);
+            Console.WriteLine(i);
+            Console.WriteLine(b);
+        }
+    }
+}
