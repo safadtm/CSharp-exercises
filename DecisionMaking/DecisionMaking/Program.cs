bcd7f234f987b0ac7a9cad840dcf4c4405d39259
safadtm <safadtm2004@gmail.com>
2024-07-27 07:08:12 +0530

    Decision Making

---
+++ DecisionMaking/DecisionMaking/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DecisionMaking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Decision Making
+             * if
+             * if else
+             * if else if else
+             * nestef if
+             */
+
+            int age = 20;
+            bool isBlind = true;
+            if (age>18)
+            {
+                Console.WriteLine("You can apply for driving license");
+            }else if (age==18)
+            {
+                if (isBlind==true) {
+                    Console.WriteLine("You cannot drive");
+                }
+                Console.WriteLine("You can start learning driving");
+            }
+            else
+            {
+
+                Console.WriteLine("You must be atleast 18 to aplly for a license");
+            }
+
+        }
+    }
+}
