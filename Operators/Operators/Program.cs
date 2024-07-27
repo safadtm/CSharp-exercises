f060da410b93e3cd2e7bfa52b099575ef4884392
safadtm <safadtm2004@gmail.com>
2024-07-27 07:01:33 +0530

    Operators

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Operators
+             * Assignment operator =
+             * Arithmetic operators +,-,*,/,%
+             * Comparison operator ==,!=,>,<,<=,>=
+             * Conditional operators &&,||
+             * Ternary operator ? :
+             */
+
+            int number1 = 100;
+            int number2 = 25;
+
+            if (number1!=number2||number1!=100)
+            {
+                Console.WriteLine("Condition 1 is true");
+            }
+            else
+            {
+                Console.WriteLine("Condition 2 is true");
+            }
+
+            bool isNumberOneIsLarger = number1 > number2 ? true : false;
+
+            Console.WriteLine(isNumberOneIsLarger);
+        }
+    }
+}
