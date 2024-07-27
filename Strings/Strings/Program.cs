6568f3e63a72cbd12c1d3b821884be4969a3a92c
safadtm <safadtm2004@gmail.com>
2024-07-27 06:53:12 +0530

    Strings

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string s = "Hello WOrld";
+            Console.WriteLine(s);
+
+            string s2 = "Escape \"Statement\"";
+            Console.WriteLine(s2);
+
+            string s3 = "C:\\Users\\DOTNET>"; // Escape sequence
+            Console.WriteLine(s3);
+
+            string s4 = @"C:\Users\DOTNET>"; // verbatim literals
+            Console.WriteLine(s4);
+
+            Console.WriteLine(s4.Length);
+            Console.WriteLine(s4.ToUpper());
+             Console.WriteLine(s4.ToUpper());
+            Console.WriteLine(s4[0]);
+            Console.WriteLine(s4[1]);
+
         }
     }
 }
--- Strings/Strings/obj/Debug/net8.0/Strings.AssemblyInfo.cs
+++ Strings/Strings/obj/Debug/net8.0/Strings.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Strings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+799196027edaea3aee1cf3d0457663b6a0e26f2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+711f2be8dc7c761c533cc1e115586913a7faf329")]
 [assembly: System.Reflection.AssemblyProductAttribute("Strings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Strings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
