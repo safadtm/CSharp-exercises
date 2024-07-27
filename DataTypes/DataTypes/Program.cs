711f2be8dc7c761c533cc1e115586913a7faf329
safadtm <safadtm2004@gmail.com>
2024-07-27 06:19:48 +0530

    Datatypes

--- DataTypes/DataTypes/Program.cs
+++ DataTypes/DataTypes/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace DataTypes
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DataTypes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /*
+            /* Data Types in C#
              * 1. Value Data Type
              *    a. Predefined Data Type
              *    b. User Defined Data Type
@@ -14,7 +16,8 @@
              *    b. User Defined Data Type
              */
 
-
+           // *1.Value Data Type
+           // *a.Predefined Data Type
             char c = 'A';
             int i = 1200;
             bool b = true;
