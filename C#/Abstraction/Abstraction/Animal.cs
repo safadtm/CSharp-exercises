2fac293b6fce0af785ce7c4853ba5ba0af33c5d7
safadtm <safadtm2004@gmail.com>
2024-07-29 15:02:21 +0530

    Abstraction

---
+++ C#/Abstraction/Abstraction/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Animal
+    {
+        public abstract void MakeSound();
+
+        public void Run()
+        {
+            Console.WriteLine("Animals can run");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bow Bow");
+        }
+    }
+}
---
+++ C#/Abstraction/Abstraction/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * OOPS
+             * ABstraction
+             */
+            
+            Dog animal = new Dog();
+            animal.MakeSound();
+            animal.Run();
+        }
+    }
+}
