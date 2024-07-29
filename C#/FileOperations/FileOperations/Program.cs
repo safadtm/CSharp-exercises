8bb01c6a4757f1e9f36eec738d5a313d35d7aeb3
safadtm <safadtm2004@gmail.com>
2024-07-29 16:06:30 +0530

    File Operations

---
+++ C#/FileOperations/FileOperations/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FileOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * File Operation - common methods
+             * 1. WriteAllText() 
+             * 2. ReadAllText() 
+             * 3. Create()
+             * 4. Delete()
+             * 5. Exists()
+             * 6. AppendText()
+             * 7. Replace()
+             * 8. Copy()
+             */
+
+
+            string contents = "My file content";
+            File.WriteAllText("myfile.txt", contents);
+
+            Console.WriteLine(File.ReadAllText("myfile.txt"));
+        }   
+    }
+}
