f86391873e3a2252b7951734986be282ec8e3e39
safadtm <safadtm2004@gmail.com>
2024-07-29 08:14:43 +0530

    break,continue

---
+++ BreakContinue/BreakContinue/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BreakContinue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * break, continue
+             * 
+             * 
+             */
+
+            for (int i = 0;i<=10;i++)
+            {
+                Console.WriteLine(i);
+                if (i==5)
+                {
+                    break;
+                }
+            }
+
+            for (int j = 10; j <= 20; j++)
+            {
+                
+                if (j == 15)
+                {
+                    continue;
+                }
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
