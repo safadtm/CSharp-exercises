e748e442bde8cbceadd27d9b47a962dc9280533a
safadtm <safadtm2004@gmail.com>
2024-07-29 15:15:20 +0530

    Interfaces

---
+++ C#/Interfaces/Interfaces/ChatSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IChatSystem
+    {
+        void SendMessage();
+        void ReceiveMessage();
+    }
+
+    class Whatsapp : IChatSystem
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Sending message via Whatsapp");
+        }
+
+        public void ReceiveMessage()
+        {
+            Console.WriteLine("Receiving message via Whatsapp");
+        }
+    }
+
+    class Telegram : IChatSystem
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Sending message via Telegram");
+        }
+
+        public void ReceiveMessage()
+        {
+            Console.WriteLine("Receiving message via Telegram");
+        }
+    }
+}
---
+++ C#/Interfaces/Interfaces/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * INTERFACES
+             * An interface defines a contract
+             * 
+             */
+
+            IChatSystem w = new Whatsapp();
+            w.SendMessage();
+            w.ReceiveMessage();
+
+            IChatSystem tele = new Telegram();
+            tele.SendMessage();
+            tele.ReceiveMessage();
+        }
+    }
+}
