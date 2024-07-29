810a91ea11ae3a603a319dba5a9aa0cf052c4e0c
safadtm <safadtm2004@gmail.com>
2024-07-29 15:56:53 +0530

    Generics

---
+++ C#/Generics/Generics/GenericDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class GenericDatabase<T>
+    {
+        public List<T> Items = new List<T>();
+
+        public void Create(T item)
+        {
+            Items.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            Items.Remove(item);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("List of " + typeof(T).Name + "'s");
+
+            Items.ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+        }
+    }
+}
---
+++ C#/Generics/Generics/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Generics
+             * Helps to decouple logic and data type
+             * code reusability
+             * create the logic in a very "Generic data type" then later
+             * apply it to many data types
+             */
+           
+            GenericDatabase<int> gidb = new GenericDatabase<int>();
+            gidb.Create(23);
+            gidb.Create(45);
+            gidb.Display();
+
+
+            GenericDatabase<string> gsdb = new GenericDatabase<string>();
+            gsdb.Create("Rohan");
+            gsdb.Create("Vivek");
+            gsdb.Display();
+        }
+    }
+}
