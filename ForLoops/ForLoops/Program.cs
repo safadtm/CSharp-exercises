1703bcd008c8cd479e74bc7520a3552148f7cacf
safadtm <safadtm2004@gmail.com>
2024-07-29 08:04:25 +0530

    For Loop

---
+++ ForLoops/ForLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * For Loops
+             * 
+             * 
+             */
+
+
+            for (int i = 0;i<=10;i++)
+            {
+                Console.WriteLine("The value {0}",i);
+            }
+            for (int i = 10; i >0; i--)
+            {
+                Console.WriteLine("The value {0}", i);
+            }
+        }
+    }
+}
