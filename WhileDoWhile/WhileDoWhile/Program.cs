26cb0ba0aa8c2cb22ee2e0e4eccde2aafdd3e18c
safadtm <safadtm2004@gmail.com>
2024-07-29 08:10:38 +0530

    While, Do While

---
+++ WhileDoWhile/WhileDoWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace WhileDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * While, Do While
+             * 
+             * 
+             */
+
+            int i = 0;
+            while (i<5)
+            {
+                Console.WriteLine(i);
+                i = i + 2;
+            }
+
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            } while (j < 5);
+        }
+    }
+}
