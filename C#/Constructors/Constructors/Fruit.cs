351bb1af1d0a5cd4d8f16c193e209cdf9a2469b9
safadtm <safadtm2004@gmail.com>
2024-07-29 09:31:09 +0530

    Constructors

---
+++ C#/Constructors/Constructors/Fruit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Fruit
+    {
+        public string color;
+        public string name;
+        public int price;
+
+        public Fruit(string FruitColor, string FruitName) {
+            color = FruitColor;
+            name = FruitName;
+        }
+
+        public Fruit(string FruitColor, string FruitName,int FruitPrice)
+        {
+            color = FruitColor;
+            name = FruitName;
+            price = FruitPrice;
+        }
+
+        public void DisplayName() {
+            Console.WriteLine("Fruit Name : "+name);
+        }
+
+        public void DisplayColor()
+        {
+            Console.WriteLine("Fruit Color : "+color);
+        }
+
+        public void DisplayPrice()
+        {
+            Console.WriteLine("Fruit Price : " + price);
+        }
+    }
+}
---
+++ C#/Constructors/Constructors/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * OOPS
+             * Constructors
+             *
+             */
+       
+           Fruit mango = new Fruit("Yellow","Mango");
+
+            mango.DisplayName();
+            mango.DisplayColor();
+
+            Fruit apple = new Fruit("Red", "Apple",30);
+
+            apple.DisplayName();
+            apple.DisplayColor();
+            apple.DisplayPrice();
+
+            Fruit pineapple = new Fruit("Yellow", "Pineapple", 60);
+
+            pineapple.DisplayName();
+            pineapple.DisplayColor();
+            pineapple.DisplayPrice();
+        }   
+    }
+}
