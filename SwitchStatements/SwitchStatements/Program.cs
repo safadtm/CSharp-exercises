a5057e18bbf3729b486fe66ab6fd53468d506f98
safadtm <safadtm2004@gmail.com>
2024-07-29 08:18:39 +0530

    Switch Statements

---
+++ SwitchStatements/SwitchStatements/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SwitchStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * switch statements
+             * 
+             * 
+             */
+            int number = 121;
+            switch (number%2)
+            {
+                case 0:
+                    Console.WriteLine("Number is even");
+                    break;
+                case 1:
+                    Console.WriteLine("Number is odd");
+                    break;
+                    default:
+                    Console.WriteLine("Wrong input");
+                    break;
+            }
+        }
+    }
+}
