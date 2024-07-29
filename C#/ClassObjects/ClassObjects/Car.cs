7474489a1091302f5b02acd15dc7f3ebd6c10122
safadtm <safadtm2004@gmail.com>
2024-07-29 09:00:03 +0530

    Class and Object

---
+++ C#/ClassObjects/ClassObjects/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjects
+{
+    public class Car
+    {
+        public string color;
+        public double price;
+    }
+}
---
+++ C#/ClassObjects/ClassObjects/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /*
+            * OBJECT ORIENTED PROGRAMMING - INTRODUCTION
+            * CLASS & OBJECTS
+            * class - Fruit
+            * objects - apple, orange etc
+            */
+
+            Fruit apple =  new Fruit();
+            apple.color = "Red";
+            Console.WriteLine(apple.color);
+
+            Fruit  orange=new Fruit();
+            orange.color = "Orange";
+            Console.WriteLine(orange.color);
+
+
+            Car ferrari = new Car();
+            ferrari.color = "Yellow";
+            ferrari.price = 155500.00;
+            Console.WriteLine(ferrari.color);
+            Console.WriteLine(ferrari.price);
+        }
+    }
+}
