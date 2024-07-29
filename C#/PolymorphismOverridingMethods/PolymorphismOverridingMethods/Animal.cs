e151d0d70f99dbb5b10a375b8337a6b6ef999289
safadtm <safadtm2004@gmail.com>
2024-07-29 10:37:10 +0530

    Polymorphism and Overriding Methods

---
+++ C#/PolymorphismOverridingMethods/PolymorphismOverridingMethods/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismOverridingMethods
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("All animals makes sound");
+        }
+    }
+
+    public class Dog:Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bow Bow!!");
+        }
+    }
+}
---
+++ C#/PolymorphismOverridingMethods/PolymorphismOverridingMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PolymorphismOverridingMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * OOPS
+             * Polymorhism and Overriding Methods 
+             */
+
+            Animal animal = new Animal();
+            animal.MakeSound();
+
+            Animal dog = new Dog();
+            dog.MakeSound();
+
+                 
+        }
+    }
+}
