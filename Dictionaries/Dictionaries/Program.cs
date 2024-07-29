97f037a0d87ab504197317d7f7fc8fa8a5d2e753
safadtm <safadtm2004@gmail.com>
2024-07-29 07:27:28 +0530

    Dictionaries

---
+++ Dictionaries/Dictionaries/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Dictionaries
+             * 
+             * 
+             * 
+             */
+
+            Dictionary<string, long> phoneBook = new Dictionary<string, long>();
+
+            phoneBook.Add("Steve",987456321);
+            phoneBook["Paul"] = 456321;
+
+            if (phoneBook.ContainsKey("Steve"))
+            {
+                Console.WriteLine("Steve number is " + phoneBook["Steve"]);
+                Console.WriteLine(phoneBook.Count);
+            }
+        }
+    }
+}
