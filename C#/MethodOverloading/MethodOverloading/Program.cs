961ccbab5daad345c675563b7de3f1c053e2c81d
safadtm <safadtm2004@gmail.com>
2024-07-29 08:44:37 +0530

    Method Overloading

---
+++ C#/MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MethodOverloading
+{
+    internal class Program
+    {
+
+        static int add(int x, int y) {
+        return x + y;
+        }
+
+        static double add(double x, double y)
+        {
+            return x + y;
+        }
+
+        static int add(int x, int y,int z)
+        {
+            return x + y+z;
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+             * Method Overloading
+             * Multiple methods can have the same name with different parameters
+             */
+            Console.WriteLine(add(56,44));
+            Console.WriteLine(add(45.36, 44.21));
+            Console.WriteLine(add(100, 200,300));
+        }
+    }
+}
--- C#/Methods/Methods/Program.cs
+++ C#/Methods/Methods/Program.cs
@@ -2,9 +2,38 @@
 {
     internal class Program
     {
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hellooo");
+        }
+
+        static int LargestNum(int num1,int num2)
+        {
+            int result; // local variable to the method
+
+            if (num1>num2) { 
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             * Methods
+             * 
+             * 
+             * 
+             */
+            SayHello();
+            SayHello();
+            Console.WriteLine(LargestNum(156, 2));
         }
     }
 }
