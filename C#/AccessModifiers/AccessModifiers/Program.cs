8a8593b00080f8aba99609cae761cdbc85aeffe1
safadtm <safadtm2004@gmail.com>
2024-07-29 15:37:38 +0530

    Access Modifiers

---
+++ C#/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Access Modifiers
+             * 1. public - code is visible for all the classes
+             * 2. private - code is visible only within the same class
+             * 3. protected - code is visible within the same class or the classes which inherits from that class
+             * 4. internal - code is only accessible within its own assesmbly
+             */
+
+            Student student = new Student();    
+            Console.WriteLine(student.name);
+            Console.WriteLine(student.GetAge());
+
+            College1 c1 = new College1();
+            Console.WriteLine(c1.GetLocation());
+
+
+        }
+    }
+}
---
+++ C#/AccessModifiers/AccessModifiers/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+     class Student
+    {
+        public string name = "Safad";
+        private int age = 20;
+        protected string location = "Kochi";
+
+        public int GetAge()
+        {
+            return age;
+        }
+    }
+
+    class College1 : Student { 
+       public string GetLocation()
+        {
+
+        return location; 
+        }
+    }
+    
+}
