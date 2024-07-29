18e823a7443cee7c42c03bf41b4742628ec51d79
safadtm <safadtm2004@gmail.com>
2024-07-29 15:23:17 +0530

    Enums

---
+++ C#/Enums/Enums/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Enums
+{
+    internal class Program
+    {
+
+        public enum Color
+        {
+            Red = 100,
+            Green,
+            Blue,
+            MainColor = Red
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Color.Red);
+            Console.WriteLine(Color.Green);
+            Console.WriteLine(Color.Blue);
+            Console.WriteLine(Color.MainColor);
+            Console.WriteLine((int)Color.Red);
+            Console.WriteLine((int)Color.Green);
+            Console.WriteLine((int)Color.Blue);
+            Console.WriteLine((int)Color.MainColor);
+        }
+    }
+}
