5f7cca1066fcb4ada3a7e1cc184f5490e6da0b94
safadtm <safadtm2004@gmail.com>
2024-07-29 10:09:14 +0530

    Inheritance

---
+++ C#/Inheritance/Inheritance/Mobile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Mobile // Parent class
+    {
+        public string os = "Andriod";
+
+        public void MakeCalls()
+        {
+            Console.WriteLine("Any mobile can make calls");
+        }
+    }
+}
---
+++ C#/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /*
+            * OOPS
+            * Inheritance
+            */
+
+            Andriod andriod = new Andriod();    
+            andriod.MakeCalls();
+        }
+
+    }
+}
