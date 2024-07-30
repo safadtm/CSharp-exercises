35d98ed0b409c37928395b707e5c725b2c97566c
safadtm <safadtm2004@gmail.com>
2024-07-30 09:40:17 +0530

    Exception Handling

---
+++ C#/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Exception handling
+             * try catch
+             * finally - block of code which runs finally whether error occurs or not
+             */
+            try
+            {
+                int[] nums = { 1, 4, 5, 6, 7, 9 };
+                Console.WriteLine(nums[10]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally {
+                Console.WriteLine("The final block executed");
+            }
+        }
+    }
+}
