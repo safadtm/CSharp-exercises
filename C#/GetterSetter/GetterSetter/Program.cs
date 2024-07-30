fd2aa7ada1fb73a3f84822d8074c7b759fba00a4
safadtm <safadtm2004@gmail.com>
2024-07-30 09:57:13 +0530

    Getter And Setter

---
+++ C#/GetterSetter/GetterSetter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GetterSetter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Getter 
+             * Setter
+             * 
+             */
+
+            Student s = new Student("Safad","Male");
+            Console.WriteLine(s.Gender);
+        }
+    }
+}
---
+++ C#/GetterSetter/GetterSetter/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetterSetter
+{
+    class Student
+    {
+        public string name;
+        private string gender;
+
+        public Student(string cName, string cGender)
+        {
+            name = cName;
+            Gender = cGender;
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return gender;
+            }
+            set
+            {
+                if (value == "Male" || value == "Female")
+                {
+                    gender = value;
+
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid value supplied");
+                }
+            }
+        }
+    }
+}
