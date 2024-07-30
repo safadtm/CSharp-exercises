59906c7bc747c4c13521ba6c46c9e51de1618d0b
safadtm <safadtm2004@gmail.com>
2024-07-30 12:12:56 +0530

    Strings Vs StringBuilder

---
+++ C#/StringsVsStringBuilder/StringsVsStringBuilder/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace StringsVsStringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * String
+             */
+            string msg = "Hello World";
+            GCHandle gch = GCHandle.Alloc(msg, GCHandleType.Pinned);    
+            IntPtr pObj = gch.AddrOfPinnedObject();
+            Console.WriteLine($"Memory address str with first value:{pObj}");
+            
+            msg += "hi";
+            gch = GCHandle.Alloc(msg,GCHandleType.Pinned);
+            pObj = gch.AddrOfPinnedObject();
+            Console.WriteLine($"Memory address str with append value:{pObj}");
+
+
+
+            /*
+             * String builder
+             * 
+             */
+
+            StringBuilder sb = new StringBuilder("");
+            sb.Append("Hello World");
+            gch = GCHandle.Alloc(sb, GCHandleType.Pinned);
+            pObj = gch.AddrOfPinnedObject();
+            Console.WriteLine($"Memory address str with append value:{pObj}");
+
+            sb.Append("hai");
+            gch = GCHandle.Alloc(sb, GCHandleType.Pinned);
+            pObj = gch.AddrOfPinnedObject();
+            Console.WriteLine($"Memory address str with append value:{pObj}");
+
+        }
+    }
+}
