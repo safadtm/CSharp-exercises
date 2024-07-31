f759dfc946949421dc953b3ee354f5ad08cc30cf
safadtm <safadtm2004@gmail.com>
2024-07-31 10:35:53 +0530

    Structs

---
+++ C#/Structs/Structs/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Structs
+{
+    internal class Program
+    {
+        struct Student
+        {
+            
+            public string name;
+            public int marks;
+
+            public Student(string name, int marks) { 
+               this.name = name;
+                this.marks = marks;
+            
+            }
+
+            public void DisplayStudent()
+            {
+                Console.WriteLine("Name : {0}, Mark : {0}",name,marks);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Student s1=new Student("Safad",70);
+           
+            s1.DisplayStudent();
+        }
+    }
+}
