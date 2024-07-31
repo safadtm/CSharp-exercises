6fa67b4fe3378e3a933724863dd7bc1ee65f439e
safadtm <safadtm2004@gmail.com>
2024-07-31 12:33:49 +0530

    Anonymous Method

---
+++ C#/AnonymousMethod/AnonymousMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AnonymousMethod
+{
+    internal class Program
+    {
+        public delegate int SumDelegate(int x,int y); 
+
+        static void Main(string[] args)
+        {
+            SumDelegate sd = delegate (int x,int y) { return x + y; };
+
+            Console.WriteLine(sd(200,300));
+        }
+    }
+}
