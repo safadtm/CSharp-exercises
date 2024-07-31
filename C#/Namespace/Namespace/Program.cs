bd519ec7faa2e04e7190ad9782ce843542077f58
safadtm <safadtm2004@gmail.com>
2024-07-31 11:05:56 +0530

    Namespace

---
+++ C#/Namespace/Namespace/Program.cs
@@ -0,0 +1,43 @@
+﻿using MyNameSpaceOne;
+using OuterNameSpace.MyNameSpaceTwo;
+
+namespace MyNameSpaceOne
+{
+    class A
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("Message from MyNameSpaceOne class A");
+        }
+    }
+}
+
+namespace OuterNameSpace
+{
+    namespace MyNameSpaceTwo
+    {
+        class B
+        {
+            public void MyMethod()
+            {
+                Console.WriteLine("Message from MyNameSpaceTwo class A");
+            }
+        }
+    }
+}
+
+
+namespace Namespace
+{
+    internal class Program
+    {
+         public static void Main(string[] args)
+        {
+           A a = new A(); 
+            a.MyMethod();
+
+            B b = new B();
+            b.MyMethod();
+        }
+    }
+}
